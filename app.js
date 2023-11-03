fca097cf492f5b6c6cc3d8bc97314a04e9cd2b87
leemingoo <dwmapi@naver.com>
2023-11-03 12:47:32 +0900

    bug: Router.use() 오류 requires a middleware function

--- app.js
+++ app.js
@@ -2,18 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
-const path = require('path');
-// const routes = require('./routes');
+const routes = require('./routes/routes');
 
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-// app.use('/', routes);
+app.use('/', routes);
 
 server.listen(port, () => {
   console.log(`http://localhost:8080`);
