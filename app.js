923e7db770554097f0bcb6b8f3e62f35be68a529
leemingoo <dwmapi@naver.com>
2023-11-03 12:42:01 +0900

    fix: router 분리 취소로 임시 해결

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
+const http = require('http');
+const server = http.createServer(app);
+
+const path = require('path');
+// const routes = require('./routes');
 
 const port = 8080;
 
-app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// app.use('/', routes);
+
+server.listen(port, () => {
   console.log(`http://localhost:8080`);
 }); 
