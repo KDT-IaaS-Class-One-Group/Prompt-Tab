5dc654933f205b625502bcdcf98f273e6b086dd3
leemingoo <dwmapi@naver.com>
2023-11-03 17:43:01 +0900

    feat: sendMessage 함수 작성 axios를 이용한 통신

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,23 @@ const txtInput = document.getElementById("txt-Input");
 const btnSubmit = document.getElementById("btn-Submit");
 const boxOutput = document.getElementById("box-Output");
 
+function sendMessage() {
+  if (txtInput.value.trim() !== "") {
+    const messageElement = document.createElement("li");
 
+    boxOutput.appendChild(messageElement);
+    // boxOutput.scrollTop = boxOutput.scrollHeight; // css 나중에 적용
+    txtInput.value = "";
+
+    axios.post("/submit", { message: txtInput.value })
+      .then(response => {
+        console.log("서버로부터 받은 응답:", response.data);
+      })
+      .catch(error => {
+        console.error("에러 발생:", error);
+      });
+  }
+}
 
 btnSubmit.addEventListener("click", sendMessage);
 txtInput.addEventListener("keyup", (e) => {
