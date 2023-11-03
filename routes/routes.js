fcb507c09b7263b3e943929917c37c7f295353b4
leemingoo <dwmapi@naver.com>
2023-11-03 12:55:34 +0900

    feat: 라우터 분리 완료

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
+const express = require('express')
+const router = express.Router();
 const path = require('path');
 
-router.use(path.join(__dirname, 'public'));
+router.use(express.static(path.join(__dirname, '../public')));
 
 router.get('/', (req, res) => {
-  console.log('gg');
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 module.exports = router;
