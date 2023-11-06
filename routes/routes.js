5fdc21f13f01d651537d115065510caa9cf20c56
leemingoo <dwmapi@naver.com>
2023-11-06 14:28:41 +0900

    feat: style.json 불러오기 시작

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,19 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
-const styles = require('../data/style.json')
+const styles = require('../data/style.json');
+
+router.get('/styles', (req, res) => {
+  res.json(styles);
+});
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-router.get('/style', (req, res) => {
-  res.json(styles);
-});
-
 router.post('/submit', (req, res) => {
   const message = req.body.message;
-  console.log("전송 받은 메시지 : ", message);
+  console.log("전송 받은 메시지: ", message);
   res.send("메시지를 성공적으로 받았습니다.");
 });
 
