b17ad5ce6061d813c75acd13bf90e2e72831d4c4
leemingoo <dwmapi@naver.com>
2023-11-06 14:19:57 +0900

    refactor: 서버 리팩토링

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
 const routes = require('./routes/routes');
 
 const port = 8080;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
-app.post('/submit', routes);
 
 server.listen(port, () => {
   console.log(`http://localhost:8080`);
--- routes/routes.js
+++ routes/routes.js
@@ -2,20 +2,20 @@ const express = require('express')
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
-
-router.use(express.static(path.join(__dirname, '../public')));
+const styles = require('../data/style.json')
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.get('/style', (req, res) => {
+  res.json(styles);
+});
 
 router.post('/submit', (req, res) => {
   const message = req.body.message;
   console.log("전송 받은 메시지 : ", message);
-  res.send("오냐 잘 받았다");
+  res.send("메시지를 성공적으로 받았습니다.");
 });
 
 module.exports = router;
