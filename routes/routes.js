5d6dd9db72f31c344b553896109e6ef99afa5852
leemingoo <dwmapi@naver.com>
2023-11-07 10:51:34 +0900

    fix: import { fileURLToPath } from 'url'; 로 해결

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
@@ -9,12 +13,12 @@ router.get('/', (req, res) => {
 
 router.get('/data.json', (req, res) => {
   // 이곳에서 JSON 파일의 경로를 설정해야 합니다
-  res.sendFile(path.join(__dirname, 'path_to_data.json'));
+  res.sendFile(path.join(__dirname, '../data', 'data.json'));
 });
 
 router.get('/style.json', (req, res) => {
   // 이곳에서 style.json 파일의 경로를 설정해야 합니다
-  res.sendFile(path.join(__dirname, 'path_to_style.json'));
+  res.sendFile(path.join(__dirname, '../data', 'style.json'));
 });
 
 router.post('/submit', (req, res) => {
