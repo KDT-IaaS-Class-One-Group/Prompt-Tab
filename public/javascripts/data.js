9c7aca6dcf40f92eb89d7c7cb23f16d62b02786e
leemingoo <dwmapi@naver.com>
2023-11-07 16:14:23 +0900

    test: 오류 테스트

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -2,6 +2,9 @@ axios.get('/data.json')
   .then(response => {
     const data = response.data;
 
+    document.getElementById('home').innerText = data.header.hamburgerMenu[0];
+    document.getElementById('good').innerText = data.header.hamburgerMenu[1];
+    document.getElementById('bad').innerText = data.header.hamburgerMenu[2];
     document.getElementById('logo').innerText = data.header.logo;
     document.getElementById('siteName').innerText = data.header.siteName;
     document.getElementById('name').innerText = data.mainContent.userInfo.name;
